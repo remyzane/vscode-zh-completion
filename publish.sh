89946e5dc4cf1f3db9f4a93a039ef3edd4cd73bd
Remy Zane <remyzane@icloud.com>
2025-11-22 14:38:26 +0800

    🌍 部署：优化打包和发布命令

--- publish.sh
+++ publish.sh
@@ -5,8 +5,6 @@ echo "=========================="
 echo "请输入 VSCode 令牌（如果只打包不发布，则直接回车）："
 read token
 
-vsce publish --no-dependencies -p $token
-
 if [ -z "$token" ]; then
   # 如果 token 为空，则只打包，不发布
   vsce package --no-dependencies
