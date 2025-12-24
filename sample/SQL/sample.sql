b867a8798eaf5a17a8c552dded1811ae9056dbbe
Remy Zane <remyzane@icloud.com>
2025-12-25 04:19:31 +0800

    🧪 测试：中文代码样例

---
+++ sample/SQL/sample.sql
@@ -0,0 +1,5 @@
+CREATE TABLE 学生表 ( id INTEGER, 姓名 TEXT, 年龄 INTEGER, 班级 TEXT );
+
+INSERT INTO 学生表 (id, 姓名, 年龄, 班级) VALUES (2, '李四', 17, '高二(1)班');
+
+SELECT id, 姓名, 年龄, 班级 FROM 学生表 WHERE 年龄 > 16 AND 班级 LIKE '高二%';
